7d7a2d0ddcaedd606f629eaf3ce4f242f3373d98
bcarranza <bayron.carranza@gmail.com>
2024-05-30 11:49:05 -0600

    blue green

---
+++ eksctl/create_cluster_green.sh
@@ -0,0 +1,7 @@
+# Command to create an Amazon EKS cluster using eksctl
+eksctl create cluster \
+    --name K8S-Green \
+    --region us-east-1 \
+    --node-type t3.micro \
+    --nodes 3 \
+    --version 1.29
