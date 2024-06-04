b256b4d907dcb45df93ea44ff1759e8f9c3f863b
bcarranza <bayron.carranza@gmail.com>
2024-06-03 23:50:29 -0600

    improvements

--- eksctl/create_cluster.sh
+++ eksctl/create_cluster.sh
@@ -2,6 +2,6 @@
 eksctl create cluster \
     --name K8S-Blue-Green \
     --region us-east-1 \
-    --node-type t3.micro \
-    --nodes 3 \
+    --node-type t3.large \
+    --nodes 2 \
     --version 1.29
